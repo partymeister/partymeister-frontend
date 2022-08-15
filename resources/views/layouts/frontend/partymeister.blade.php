b28ca9efc81e2f277fb2b941894f933b473e84b6
Reza Esmaili <bitbucket@dfox.info>
2022-08-15 10:34:30 +0200

    Fix party name

--- resources/views/layouts/frontend/partymeister.blade.php
+++ resources/views/layouts/frontend/partymeister.blade.php
@@ -4,20 +4,17 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>{{$version->name}} | {{config('motor-backend-project.name_frontend')}}</title>
+    <title>{{$version->name}} | {{config('motor-cms-frontend.name')}}</title>
 
     <link href="{{ mix('/css/motor-frontend.css') }}" rel="stylesheet" type="text/css"/>
     @yield('view-styles')
 </head>
 <body>
 @include('motor-cms::layouts.frontend.partials.navigation')
-<div class="grid-container" style="margin-bottom: 8rem;">
+<div class="grid-container" style="margin-bottom: 8rem; margin-top: 1rem;">
     @include('motor-cms::layouts.frontend.partials.template-sections', ['rows' => $template['items']])
 </div>
 <div class="columns shrink footer text-center" style="position: fixed; bottom: 0; width: 100%;">
-    <ul class="menu align-center">
-        <li><a href="https://2019.revision-party.net/privacy_policy">Privacy policy</a></li>
-    </ul>
 </div>
 <script src="{{mix('js/partymeister-frontend.js')}}"></script>
 @yield('view-scripts')
