232974df364565f2ad3fb84c705cf2f98e429fd2
Reza Esmaili <github@dfox.info>
2026-04-15 13:11:50 +0200

    chore: Laravel 13 upgrade and CMS component cleanup
    - Remove ComponentPhotowalls and associated frontend views
      (motor-cms being retired)
    - Empty motor-cms-page-components config

--- config/motor-cms-page-components.php
+++ config/motor-cms-page-components.php
@@ -1,24 +1,6 @@
 <?php
 
 return [
-    'groups' => [
-        'partymeister-frontend' => [
-            'name' => 'Partymeister frontend',
-        ],
-    ],
-    'components' => [
-        'photowalls' => [
-            'name' => 'Photowall',
-            'description' => 'Show Photowall component',
-            'view' => 'partymeister-frontend::frontend.components.photowalls-tw',
-            'component_class' => 'Partymeister\Frontend\Components\ComponentPhotowalls',
-            'compatibility' => [
-
-            ],
-            'permissions' => [
-
-            ],
-            'group' => 'partymeister-frontend',
-        ],
-    ],
+    'groups' => [],
+    'components' => [],
 ];
